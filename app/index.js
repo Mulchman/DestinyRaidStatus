43ab8ee0a33e604bba7c30dc7854180093dc2cbb
Mulchman <mulchman@gmail.com>
2017-05-08 14:37:32 -0700

    Renames

--- app/index.js
+++ app/index.js
@@ -7,21 +7,21 @@ window.MessageFormat = require('messageformat');
 // Initialize the main DRS app
 require('./scripts/app.module');
 
-require('./scripts/drsBungieLookup.service.js');
+require('./scripts/drsBungieLookup.factory.js');
 require('./scripts/drsConstants');
 require('./scripts/drsFooter.directive.js');
 require('./scripts/drsHeader.directive.js');
 require('./scripts/drsInputPlayer.directive.js');
 require('./scripts/drsMain.controller.js');
 require('./scripts/drsPlayerList.directive.js');
-require('./scripts/drsPlayerList.service.js');
-require('./scripts/drsQueue.service.js');
+require('./scripts/drsPlayerList.factory.js');
+require('./scripts/drsQueue.factory.js');
 require('./scripts/drsSettings.controller.js');
-require('./scripts/drsSettings.service.js');
-require('./scripts/drsUtilities.service.js');
+require('./scripts/drsSettings.factory.js');
+require('./scripts/drsUtilities.factory.js');
 
 // TODO: move to a separate module
 require('./scripts/api/the100/drsThe100.controller.js');
-require('./scripts/api/the100/drsThe100.service.js');
+require('./scripts/api/the100/drsThe100.factory.js');
 
 require('./scss/main.scss');
