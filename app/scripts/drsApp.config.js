25573a13dc17d048b88b6290f1d23282431e9a61
Mulchman <mulchman@gmail.com>
2018-05-25 15:55:01 -0700

    Get things compiling/running again.

--- app/scripts/drsApp.config.js
+++ app/scripts/drsApp.config.js
@@ -12,8 +12,6 @@ function config($compileProvider, $locationProvider, $httpProvider, $translatePr
                 $translateMessageFormatInterpolationProvider, localStorageServiceProvider, /* ngHttpRateLimiterConfigProvider */) {
   'ngInject';
 
-  // TODO: remove this depenency by fixing component bindings https://github.com/angular/angular.js/blob/master/CHANGELOG.md#breaking-changes-1
-  $compileProvider.preAssignBindingsEnabled(true);
   // Allow chrome-extension: URLs in ng-src
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|chrome-extension):|data:image\/)/);
 
