2e28f04c5abb0ae3806170cbbfc5960985793676
Mulchman <mulchman@gmail.com>
2018-06-21 16:27:48 -0700

    Add copy to clipboard.

--- app/scripts/drsPlayerList.D2.component.js
+++ app/scripts/drsPlayerList.D2.component.js
@@ -5,10 +5,23 @@ function PlayerListD2Ctrl(PlayerListD2Service) {
   'ngInject';
 
   const vm = this;
+  vm.copyClicked = copyClicked;
 
   angular.extend(vm, {
     pls: PlayerListD2Service
   });
+
+  function copyClicked(alias) {
+    // https://stackoverflow.com/a/43483323
+    function handler(event) {
+      event.clipboardData.setData('text/plain', alias);
+      event.preventDefault();
+      document.removeEventListener('copy', handler, true);
+    }
+
+    document.addEventListener('copy', handler, true);
+    document.execCommand('copy');
+  }
 }
 
 export const PlayerListD2Component = {
